ad43fc59106692ae77ce49cf46c6f5380fe0e69f
satya-srikar <srikar.n@valueinfotech.com>
2025-01-02 14:49:41 +0530

    ESLint and Prettier setup

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,8 @@
+import React from "react";
 import type { Metadata } from "next";
+// eslint-disable-next-line camelcase
 import { Geist, Geist_Mono } from "next/font/google";
+
 import "./globals.css";
 
 const geistSans = Geist({
